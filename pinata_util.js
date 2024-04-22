1f331ad4eb88e1b5a082283afca42cee109a5b0e
rreece15 <reece.riherd@gmail.com>
2024-04-22 18:15:56 -0500

    added pinata funcs

--- pinata_util.js
+++ pinata_util.js
@@ -32,4 +32,39 @@ const pinFileToIPFS = async () => {
       console.log(error);
     }
 }
+
+export const uploadJSONToIPFS = async(JSONBody) => {
+  const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
+  //making axios POST request to Pinata ⬇️
+  return axios 
+      .post(url, JSONBody, {
+          headers: {
+              pinata_api_key: key,
+              pinata_secret_api_key: secret,
+          }
+      })
+      .then(function (response) {
+         return {
+             success: true,
+             pinataURL: "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash
+         };
+      })
+      .catch(function (error) {
+          console.log(error)
+          return {
+              success: false,
+              message: error.message,
+          }
+
+  });
+};
+
+
+export const GetIpfsUrlFromPinata = (pinataUrl) => {
+  var IPFSUrl = pinataUrl.split("/");
+  const lastIndex = IPFSUrl.length;
+  IPFSUrl = "https://ipfs.io/ipfs/"+IPFSUrl[lastIndex-1];
+  return IPFSUrl;
+};
+
 pinFileToIPFS()
